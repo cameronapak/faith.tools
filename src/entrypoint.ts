696c354db163b9bd9dd8542104a68cd5b91d997a
Cameron Pak <cameronandrewpak@gmail.com>
2024-03-10 12:49:09 -0500

    Initial commit

---
+++ src/entrypoint.ts
@@ -0,0 +1,13 @@
+import type { Alpine } from 'alpinejs';
+// @ts-ignore - Has no associated types.
+import intersect from '@alpinejs/intersect';
+// @ts-ignore - Has no associated types.
+import persist from '@alpinejs/persist';
+// @ts-ignore - Has no associated types.
+import collapse from '@alpinejs/collapse';
+
+export default (Alpine: Alpine) => {
+  Alpine.plugin(intersect);
+  Alpine.plugin(persist);
+  Alpine.plugin(collapse);
+}
