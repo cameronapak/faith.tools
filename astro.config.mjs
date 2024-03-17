8a93c23f25f3a808daaa322f57d258e8d332fa6c
Cameron Pak <cameronandrewpak@gmail.com>
2024-03-17 14:32:12 -0500

    rm unused cars and unused file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,6 @@ import keystatic from '@keystatic/astro';
 import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc';
 
-const SENTRY_DSN = import.meta.env.SENTRY_DSN;
-const SENTRY_AUTH_TOKEN = import.meta.env.SENTRY_AUTH_TOKEN;
-const SENTRY_PROJECT = import.meta.env.SENTRY_PROJECT;
-
 const integrations = [
   tailwind(),
   // This entrypoint file is where Alpine plugins are registered.
@@ -23,5 +19,5 @@ const integrations = [
 export default defineConfig({
   integrations,
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
